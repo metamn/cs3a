8e232fbc90738db57b55f5eb4511945ab6b82375
cs <bartus.csongor@gmail.com>
2014-08-11 11:37:23 +0300

    Menu done

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
-  // Menu
+  // Click on menu
   $('header').click(function() {
     $('header').toggleClass('active');
   });
+  
+  // Mark active menu item
+  $("#menu ul li").first().addClass('active');
 });
