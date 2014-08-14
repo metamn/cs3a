c4da848c10019e3026df360c6903d70b0525e7a5
cs <bartus.csongor@gmail.com>
2014-08-14 10:58:46 +0300

    Tooltips added on frontpage

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+  
+  // Tooltip 
+  $('.tooltip-item').click(function() {
+    $('.tooltip').html($(this).find('.tooltip-text').html());
+    $('.tooltip').addClass('active');
+  });
 
-  // Click on menu
+  // Click to show the menu
   $('header').click(function() {
     $('header').toggleClass('active');
   });
