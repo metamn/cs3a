11f301b7c09d137d82b9ef6cc5c4fd79819ddb99
cs <bartus.csongor@gmail.com>
2014-08-15 10:25:59 +0300

    Ready to deploy

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   
   // Tooltip 
-  $('.tooltip-item').click(function() {
+  $('.tooltip-item').on('click', function() {
     $('.tooltip span').html($(this).find('.tooltip-text').html());
     $('.tooltip').addClass('active');
     
     $(this).parent().siblings().removeClass('active');
     $(this).parent().addClass('active');
   });
+  
+  $('.tooltip-item').first().trigger('click');
+  
 
   // Click to show the menu
   $('header').click(function() {
